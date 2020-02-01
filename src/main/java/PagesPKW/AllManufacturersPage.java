34f9d9fe43fe8badce9b74dc2b640f67478cf121
RomaChel <romanchelombitko@gmail.com>
2020-02-01 16:51:29 +0200

    added new test

---
+++ src/main/java/PagesPKW/AllManufacturersPage.java
@@ -0,0 +1,11 @@
+package PagesPKW;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+
+public class AllManufacturersPage {
+
+    public SelenideElement AllManufacturersBlock = $x("//div[@class='featured_manufacturers tyres_brands_listing']");
+}
